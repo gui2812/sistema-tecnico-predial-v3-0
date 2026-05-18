56e358fa04ebec24c5d03d4d20007b24f71e909c
gui2812 <guilhermesantana281203@gmail.com>
2026-05-18 14:22:27 -0300

    Update Layout.jsx

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -1,8 +1,21 @@
 import Sidebar from './Sidebar';
 import Header from './Header';
+import AssistenteTecnico from './AssistenteTecnico';
+
 export default function Layout({ children, page, setPage, user }) {
-  return <div className="min-h-screen bg-slate-100 md:flex">
-    <Sidebar page={page} setPage={setPage} user={user}/>
-    <main className="flex-1 min-w-0"><Header page={page} user={user}/><div className="p-4 md:p-8">{children}</div></main>
-  </div>
+  return (
+    <div className="min-h-screen bg-slate-100 md:flex">
+      <Sidebar page={page} setPage={setPage} user={user}/>
+
+      <main className="flex-1 min-w-0">
+        <Header page={page} user={user}/>
+
+        <div className="p-4 md:p-8">
+          {children}
+        </div>
+      </main>
+
+      <AssistenteTecnico />
+    </div>
+  );
 }
